bdcc386144a9c1ec6aa1236fdec2190bc7eae759
Lan <15669147150g@gmail.com>
2025-01-17 00:15:08 +0800

    解决无法访问

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule)
 
-  // 支持跨域访问（如果需要）
-  app.enableCors();
+    // 支持跨域访问（如果需要）
+    app.enableCors()
 
-  // 监听动态端口
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
+    // 监听动态端口
+    const port = process.env.PORT || 3000
+    // 日志输出，检查是否正常执行
+    console.log(`Server is starting on port ${port}`)
+    await app.listen(port)
 }
-bootstrap();
+bootstrap()

5f29d07d0056873288011536957ab4dd6163a5af
Lan <15669147150g@gmail.com>
2025-01-17 00:03:38 +0800

    解决无法访问

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // 支持跨域访问（如果需要）
+  app.enableCors();
+
+  // 监听动态端口
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
